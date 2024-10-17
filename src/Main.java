72bfee4145c300f8c2d2bd4f1b343e0f21cf6628
muhammadsaid <mpfpmf57@gmail.com>
2024-10-17 17:54:20 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Игрок 1, вводите своё имя: ");
+        String player1 = in.nextLine();
+        System.out.print("Игрок 2, вводите своё имя: ");
+        String player2 = in.nextLine();
+    }
+}
